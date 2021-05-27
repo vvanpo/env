eafc15c4928f02ed68412c189b4c995103b96fc1
Victor van Poppelen <vvanpo@gmail.com>
2021-05-26 18:11:52 -0700

    Add `gitf` alias

--- aliases.bash
+++ aliases.bash
@@ -4,5 +4,6 @@ alias ls='ls --color=always' l='ls -lah' ll='l | less'
 alias grep='grep -E --color=always'
 alias ps='ps -efjH | less'
 
-alias gits='git status'
 alias gitd='git diff HEAD'
+alias gitf='git fetch --prune'
+alias gits='git status'
