9c9f31746ebeb089970de95a0c386e2323ac4fdf
Victor van Poppelen <vvanpo@gmail.com>
2024-01-10 10:02:45 +0100

    Fix Bash prompt lag on WSL2

--- prompt.bash
+++ prompt.bash
@@ -10,7 +10,16 @@ prefix="${yellow}\D{%H:%M} ${bold}${green}\u${reset}:"
 [[ $SSH_CLIENT ]] && prefix="ssh://${bold}${green}\u${default}@${red}\H${reset}:"
 postfix=" ${bold}${blue}\w\n\$${reset} "
 
-PS1="${prefix}\$(__git_ps1)${postfix}"
+# Running git from a Windows mount in WSL2 is slow, causing each prompt to hang for a second.
+function _git_ps1_proxy {
+	if [[ $WSL_DISTRO_NAME && $(pwd -P) != /mnt/* ]]; then
+		__git_ps1
+	else
+		echo -n " (windows /mnt)"
+	fi
+}
+
+PS1="${prefix}\$(_git_ps1_proxy)${postfix}"
 # Secondary prompt for multi-line commands.
 PS2="  ${blue}>${reset} "
 # Trims the path to 3 components in the PS1, replacing it with an ellipsis.
